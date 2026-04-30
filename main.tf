ae63bced93590704bd73d7b0291fedaa6ff28079
Saymon Felipe <sayfelanjos@hotmail.com>
2026-04-30 17:51:14 -0300

    Change infra from Azure IoTHub to Azure VM

--- main.tf
+++ main.tf
@@ -1,166 +1,142 @@
-terraform {
-  required_version = ">= 1.2.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.68.0" # Atenção: Verifique se a v4.x já está estável para estes recursos
-    }
-  }
-}
-
+# 1. Configuração do Provider
 provider "azurerm" {
   features {}
 }
 
-resource "random_id" "storage_suffix" {
-  byte_length = 4
+# 2. Grupo de Recursos
+resource "azurerm_resource_group" "astro_rg" {
+  name     = "Astro-Econ-RG"
+  location = "East US" # Pode alterar para a região da sua preferência
 }
 
-# --- GRUPO DE RECURSOS ---
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+# 3. Infraestrutura de Rede
+resource "azurerm_virtual_network" "vnet" {
+  name                = "astro-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.astro_rg.location
+  resource_group_name = azurerm_resource_group.astro_rg.name
 }
 
-# --- IOT HUB ---
-resource "azurerm_iothub" "hub" {
-  name                = var.iothub_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.astro_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-  sku {
-    name     = var.iothub_sku
-    capacity = var.iothub_units
+# 4. Endereço IP Público (para aceder ao Grafana)
+resource "azurerm_public_ip" "pip" {
+  name                = "astro-vm-ip"
+  resource_group_name = azurerm_resource_group.astro_rg.name
+  location            = azurerm_resource_group.astro_rg.location
+  sku               = "Standard"
+  allocation_method = "Static"
+}
+
+# 5. Segurança (Firewall - NSG)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "astro-nsg"
+  location            = azurerm_resource_group.astro_rg.location
+  resource_group_name = azurerm_resource_group.astro_rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Grafana"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "MQTT"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1883"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-# --- STORAGE ---
-resource "azurerm_storage_account" "storage" {
-  name                     = "astro${random_id.storage_suffix.hex}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
-}
+# 6. Interface de Rede
+resource "azurerm_network_interface" "nic" {
+  name                = "astro-nic"
+  location            = azurerm_resource_group.astro_rg.location
+  resource_group_name = azurerm_resource_group.astro_rg.name
 
-resource "azurerm_storage_container" "telemetry" {
-  name                  = var.storage_container_name
-  storage_account_id  = azurerm_storage_account.storage.id
-  container_access_type = "private"
-}
-
-# --- STREAM ANALYTICS JOB ---
-resource "azurerm_stream_analytics_job" "stream_job" {
-  name                = var.stream_job_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  streaming_units     = 1
-
-  compatibility_level  = "1.2"
-  output_error_policy  = "Stop"
-  
-  # Ajustado para bater com os nomes dos inputs/outputs abaixo
-  transformation_query = <<QUERY
-    SELECT * INTO [AstroBlobOutput] 
-    FROM [AstroIoTHubInput] 
-    TIMESTAMP BY EventEnqueuedUtcTime
-QUERY
-}
-
-# --- INPUT (IOT HUB) ---
-resource "azurerm_stream_analytics_stream_input_iothub" "input" {
-  name                         = "AstroIoTHubInput"
-  stream_analytics_job_name    = azurerm_stream_analytics_job.stream_job.name
-  resource_group_name          = azurerm_resource_group.rg.name
-  endpoint                     = "messages/events"
-  eventhub_consumer_group_name = "$Default"
-  
-  # Referenciando o recurso criado dinamicamente
-  iothub_namespace             = azurerm_iothub.hub.name
-  shared_access_policy_key     = azurerm_iothub.hub.shared_access_policy[0].primary_key
-  shared_access_policy_name    = "iothubowner"
-
-  serialization {
-    type   = "Json"
-    encoding = "UTF8"
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
-# --- OUTPUT (BLOB) ---
-resource "azurerm_stream_analytics_output_blob" "output" {
-  name                      = "AstroBlobOutput"
-  stream_analytics_job_name = azurerm_stream_analytics_job.stream_job.name
-  resource_group_name       = azurerm_resource_group.rg.name
-  
-  # Referenciando o storage criado dinamicamente
-  storage_account_name      = azurerm_storage_account.storage.name
-  storage_account_key       = azurerm_storage_account.storage.primary_access_key
-  storage_container_name    = azurerm_storage_container.telemetry.name
-  
-  path_pattern              = "ano={datetime:yyyy}/mes={datetime:MM}/dia={datetime:dd}"
-  date_format               = "yyyy-MM-dd"
-  time_format               = "HH"
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
-  serialization {
-      type   = "Json"
-      encoding = "UTF8"
-      format   = "LineSeparated"
+# 7. Máquina Virtual (Standard_B1ms - 2GB RAM / Económica)
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "Astro-Server-VM"
+  resource_group_name = azurerm_resource_group.astro_rg.name
+  location            = azurerm_resource_group.astro_rg.location
+  size                = "Standard_B1ms"
+  admin_username      = "astroadmin"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "astroadmin"
+    public_key = file("~/.ssh/id_rsa.pub") # Certifique-se de que tem a sua chave pública aqui
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
   }
 }
 
-
-resource "null_resource" "create_iot_device" {
-  depends_on = [azurerm_iothub.hub]
-
-  provisioner "local-exec" {
-    # Usamos uma linha única para evitar problemas de escape no shell do Terraform
-    command = "az extension add --name azure-iot --yes && az iot hub device-identity create --device-id 'RaspberryPiAstro' --hub-name ${azurerm_iothub.hub.name} --resource-group ${azurerm_resource_group.rg.name} --auth-method shared_private_key"
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "az iot hub device-identity delete --device-id 'RaspberryPiAstro' --hub-name ${self.triggers.hub_name} --resource-group ${self.triggers.rg_name}"
-  }
-
-  triggers = {
-    hub_name = azurerm_iothub.hub.name
-    rg_name  = azurerm_resource_group.rg.name
-  }
+# 8. DISCO DE DADOS PERSISTENTE (Onde os dados do Astro serão guardados)
+resource "azurerm_managed_disk" "data_disk" {
+  name                 = "Astro-Data-Disk"
+  location             = azurerm_resource_group.astro_rg.location
+  resource_group_name  = azurerm_resource_group.astro_rg.name
+  storage_account_type = "StandardSSD_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 32
 }
 
-resource "azurerm_dashboard_grafana" "astro_dashboard" {
-  name                              = "astro-grafana-${random_id.storage_suffix.hex}"
-  resource_group_name               = azurerm_resource_group.rg.name
-  location                          = azurerm_resource_group.rg.location
-  api_key_enabled                   = true
-  deterministic_outbound_ip_enabled = false
-  public_network_access_enabled     = true
-  grafana_major_version = "12"
-
-  identity {
-    type = "SystemAssigned"
-  }
+# 9. Anexar o Disco à VM
+resource "azurerm_virtual_machine_data_disk_attachment" "attach" {
+  managed_disk_id    = azurerm_managed_disk.data_disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
+  lun                = "10"
+  caching            = "ReadWrite"
 }
 
-# Permissão para o Grafana ler os dados do Azure Monitor/Storage
-resource "azurerm_role_assignment" "grafana_monitor" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Monitoring Reader"
-  principal_id         = azurerm_dashboard_grafana.astro_dashboard.identity[0].principal_id
-}
-
-resource "azurerm_log_analytics_workspace" "astro_law" {
-  name                = "astro-law-${random_id.storage_suffix.hex}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30 # Para o seu plano de 2 meses, podemos ajustar aqui
-}
-
-# 2. Configurar o Grafana para ter permissão de ler este Workspace
-resource "azurerm_role_assignment" "grafana_la_reader" {
-  scope                = azurerm_log_analytics_workspace.astro_law.id
-  role_definition_name = "Log Analytics Reader"
-  principal_id         = azurerm_dashboard_grafana.astro_dashboard.identity[0].principal_id
-}
