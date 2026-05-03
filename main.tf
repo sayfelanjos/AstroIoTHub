069ab9faa707d481c0048aff8736a1e76265a902
saymon <saymonfelipe@hotmail.com>
2026-05-03 17:42:42 -0300

    Add secury connection to the VM (https)

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 # 2. Grupo de Recursos
 resource "azurerm_resource_group" "astro_rg" {
   name     = "Astro-Econ-RG"
-  location = "East US" # Pode alterar para a região da sua preferência
+  location = "East US"
 }
 
 # 3. Infraestrutura de Rede
@@ -24,13 +24,15 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# 4. Endereço IP Público (para aceder ao Grafana)
+# 4. Endereço IP Público com DNS Label
 resource "azurerm_public_ip" "pip" {
   name                = "astro-vm-ip"
   resource_group_name = azurerm_resource_group.astro_rg.name
   location            = azurerm_resource_group.astro_rg.location
-  sku               = "Standard"
-  allocation_method = "Static"
+  sku                 = "Standard"
+  allocation_method   = "Static"
+  # This creates: astro-magnetometer.eastus.cloudapp.azure.com
+  domain_name_label   = "astro-magnetometer" 
 }
 
 # 5. Segurança (Firewall - NSG)
@@ -52,25 +54,25 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "Grafana"
+    name                       = "HTTP-Certbot-Challenge"
     priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "3000"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "MQTT"
+    name                       = "HTTPS"
     priority                   = 120
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "1883"
+    destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
@@ -90,12 +92,12 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "example" {
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# 7. Máquina Virtual (Standard_B1ms - 2GB RAM / Económica)
+# 7. Máquina Virtual com Let's Encrypt (Certbot) e Auto-Mount
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "Astro-Server-VM"
   resource_group_name = azurerm_resource_group.astro_rg.name
@@ -104,9 +106,57 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_username      = "astroadmin"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              # 1. Mount Persistent Disk (LUN 10)
+              DISK_DEVICE="/dev/disk/azure/scsi1/lun10"
+              MOUNT_POINT="/mnt/astro_data"
+              
+              mkdir -p $MOUNT_POINT
+              # Only format if no filesystem exists (Safe for data preservation)
+              if ! blkid $DISK_DEVICE; then
+                  mkfs.ext4 $DISK_DEVICE
+              fi
+              
+              mount $DISK_DEVICE $MOUNT_POINT
+              echo "$DISK_DEVICE $MOUNT_POINT ext4 defaults,nofail 0 2" >> /etc/fstab
+
+              # 2. Install Nginx and Certbot
+              apt-get update
+              apt-get install -y nginx certbot python3-certbot-nginx
+
+              # 3. Create Nginx config for your Azure DNS
+              cat <<EOT > /etc/nginx/sites-available/default
+              server {
+                  listen 80;
+                  server_name astro-magnetometer.eastus.cloudapp.azure.com;
+
+                  location / {
+                      proxy_pass http://localhost:3000;
+                      proxy_set_header Host \$host;
+                      proxy_set_header X-Real-IP \$remote_addr;
+                  }
+
+                  location /influx/ {
+                      proxy_pass http://localhost:8086/;
+                      proxy_set_header Host \$host;
+                      proxy_set_header X-Real-IP \$remote_addr;
+                  }
+              }
+              EOT
+
+              systemctl restart nginx
+
+              # 4. Request Let's Encrypt Cert (Non-Interactive)
+              # Note: This requires the DNS to be active. 
+              # It may take a minute for Azure DNS to propagate after creation.
+              certbot --nginx -d astro-magnetometer.eastus.cloudapp.azure.com --non-interactive --agree-tos -m admin@example.com --redirect
+              EOF
+  )
+
   admin_ssh_key {
     username   = "astroadmin"
-    public_key = file("~/.ssh/id_rsa.pub") # Certifique-se de que tem a sua chave pública aqui
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
@@ -122,7 +172,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
-# 8. DISCO DE DADOS PERSISTENTE (Onde os dados do Astro serão guardados)
+# 8. Disco de Dados Persistente
 resource "azurerm_managed_disk" "data_disk" {
   name                 = "Astro-Data-Disk"
   location             = azurerm_resource_group.astro_rg.location
@@ -139,4 +189,3 @@ resource "azurerm_virtual_machine_data_disk_attachment" "attach" {
   lun                = "10"
   caching            = "ReadWrite"
 }
-
